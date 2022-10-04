6042caf5ad513b3afe478932a8f9033a9cd3019b
pedronascimento8055 <60989489+pedronascimento8055@users.noreply.github.com>
2022-10-04 18:38:13 -0300

    Add files via upload

---
+++ exercicio_3.3.py
@@ -0,0 +1,10 @@
+# Faça Você Mesmo
+# Exercicio 3.3 Pag. 73 
+
+trasnporte = ['Vectra', 'Civic', 'HB20', 'Cruise', 'Pálio']
+message_0 = "Eu tenho um "
+message_1 = "Quero Comprar um "
+message_2 = "Acho muito bonito "
+message_3 = "Esse carro e muito economico "
+
+print("\n" + message_0 + "" + trasnporte[0] + "\n" + message_1 + " " + trasnporte[1] + "\n" + message_2 + "" + trasnporte[2] + "\n" + message_3 + "" + trasnporte[4] + "\n")
