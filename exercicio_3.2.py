492760044445c2e976db15acf76e8f327d93de4f
pedronascimento8055 <60989489+pedronascimento8055@users.noreply.github.com>
2022-10-04 18:09:21 -0300

    Add files via upload

---
+++ exercicio_3.2.py
@@ -0,0 +1,17 @@
+# Faça Você mesmo
+# 3.4 Lista de Convidados pagina 73 Livro Curso Intensivo Python Eric Matthes
+
+names = ['jéssica', 'edneia', 'matheus', 'pedro', 'robert'] # Lista de Convidados
+message_0 = "Sejá Bem Vindo " + names[0] + " Você está bem? " # Mensagem a cada convidado na lista 
+message_1 = "Que bom ter sua presença aqui " + names[1] + " Fique avontade"
+message_2 = "O finalmente você chegou " + names[2] + " E um prazer velo"
+message_3 = "A qaunto não o vejo " + names[4] + " Estou muito Feliz com sua presença"
+
+
+print("\n" + message_0.title()) # "\n" pula uma linha assi o código fica mais organizado quando printar a tela 
+print("\n" + message_1.title())
+print("\n" + message_2.title())
+print("\n" + message_3.title())
+print("\n")
+
+
